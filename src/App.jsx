486ef191b4928603eadb59f08302ac645220ea77
GhoshAditi <hena.ghosh786@gmail.com>
2024-03-31 00:30:51 +0530

    prescribe

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import {Link} from 'react-router-dom';
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
       <div>
+      <Link to="/prescription">Go to Prescription</Link>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
